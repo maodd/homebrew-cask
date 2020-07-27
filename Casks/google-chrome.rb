87857c075fe1c8cab37310511b05b750b268c8ac
Frank Mao <maodd.ca@gmail.com>
2020-07-27 14:33:21 -0600

    udpate sha.

--- Casks/google-chrome.rb
+++ Casks/google-chrome.rb
@@ -1,6 +1,6 @@
 cask "google-chrome" do
   version '80.0.3987.163'
-  sha256 '1360268ac28d31da90d02dac5fe579fdd49943fcd622f798243ff20893483d46'
+  sha256 '8ba5ffd2163698e70f1756d8a185820b06289c5dcbeaccd151303d071dbbff1a'
 
   url "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
   appcast "https://omahaproxy.appspot.com/history?os=mac;channel=stable"
